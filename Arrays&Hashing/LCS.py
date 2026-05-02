94dfec66d963d47a84a1a4ae8aa9e6185a104e07
Saintcyr77 <shivam.bhatt04@gmail.com>
2026-05-02 22:13:50 +0530

    lcs

--- Arrays&Hashing/LCS.py
+++ Arrays&Hashing/LCS.py
@@ -1,24 +1,80 @@
+#brute force o(n)
+# class Solution:
+#     def lcs(self,nums:list[int])->int:
+        
+#         hashset = set(nums)
+
+#         res = 0
+
+#         for i in range(len(sorted(nums))):
+
+#             streak, cur = 0, nums[i]
+
+#             while cur in hashset:
+#                 cur+=1
+#                 streak+=1
+
+#             res = max(streak,res)
+
+#         return res
+
+#----------------------------------------onlogn not on2 since both loops run on i and i is never reset-------------------------------#
+
+
+# class Solution:
+#     def lcs(self,nums:list[int])->int:
+
+#         if not nums:
+#             return 0
+
+#         nums.sort()
+
+#         res = 0
+
+#         streak, cur = 0, nums[0]
+
+#         i = 0
+
+#         while i<len(nums):
+
+#             if cur!=nums[i]:
+#                 cur = nums[i]
+#                 streak = 0
+
+#             while i<len(nums) and nums[i]==cur:
+#                 i+=1
+            
+#             streak+=1
+#             cur+=1
+
+#             res = max(streak,res)
+
+#         return res
+                
+           
+
 class Solution:
     def lcs(self,nums:list[int])->int:
-        
-        ans = []
+        hashset = set(nums)
 
-        count = 0
-        for i in range(len(nums)-1):
-            ans.append(count)
-            count = 0
-            for j in range(i+1,len(nums)):
-                if nums[j]==nums[i]+1:
-                    count+=1
+        longest = 0
+        counter= 0
 
-        sorted(ans,reverse=True)
+        for num in nums:
+            if num-1 not in hashset:
+                counter = 1
+            while num+counter in hashset:
+                counter+=1
+                print(counter)
 
-        print(ans)
+            longest = max(longest,counter)
 
+        return longest
+                
 
       
 
 
 sol = Solution()
 
-sol.lcs([2,20,4,10,3,4,5])
+print(sol.lcs([9,1,4,7,3,-1,0,5,8,-1,6]))
